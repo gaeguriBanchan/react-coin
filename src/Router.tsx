9b9e934a970ca190275b103324f7b0687505026e
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-13 20:31:28 +0900

    Publishing error

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,10 +9,10 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={process.env.PUBLIC_URL + '/'} element={<Coins />} />
-        <Route path={process.env.PUBLIC_URL + '/:coinId'} element={<Coin />}>
-          <Route path={process.env.PUBLIC_URL + 'chart'} element={<Chart />} />
-          <Route path={process.env.PUBLIC_URL + 'price'} element={<Price />} />
+        <Route path="/" element={<Coins />} />
+        <Route path="/:coinId" element={<Coin />}>
+          <Route path="chart" element={<Chart />} />
+          <Route path="price" element={<Price />} />
         </Route>
       </Routes>
     </BrowserRouter>
